179acfe5442eeacd91c99870fb1734e4b3bd33ab
Harsamerveer <harsamerveersingh@gmail.com>
2026-03-10 18:44:52 -0700

    Updated productRoutes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 
-router.get('/products', productController.getAllProducts);
-router.get('/products/:id', productController.getProductById);
+router.get('/', productController.getAllProducts);
+router.get('/:id', productController.getProductById);
 
 module.exports = router;
