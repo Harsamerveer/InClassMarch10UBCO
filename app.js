66e64641ac9649b22c7d6f54f6c0e52855473ae1
Harsamerveer <harsamerveersingh@gmail.com>
2026-03-10 18:46:03 -0700

    Updated app.js file

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 const productRoutes = require('./routes/productRoutes');
 
 app.use(express.json());
-app.use('/api', productRoutes);
+app.use('/api/products', productRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
